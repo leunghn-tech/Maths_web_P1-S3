259b1c4ea1e77b90a3a10b1b386d2730b5e157f6
leunghn-tech <2.64291194e+08+leunghn-tech@users.noreply.github.com>
2026-08-23 07:35:03 +0000

    feat: comprehensively expand P3 learning stations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,6 +29,7 @@ import P2P3StarterPractice from "./pages/P2P3StarterPractice";
 import P3ProgressPractice from "./pages/P3ProgressPractice";
 import P3DataPractice from "./pages/P3DataPractice";
 import P3GeometryPractice from "./pages/P3GeometryPractice";
+import P3CorePractice from "./pages/P3CorePractice";
 import P2Practice from "./pages/P2Practice";
 import P3Practice from "./pages/P3Practice";
 import P4Practice from "./pages/P4Practice";
@@ -85,6 +86,8 @@ function Router() {
       <Route path="/practice/p3-large-numbers" component={P3DataPractice} />
       <Route path="/practice/p3-charts" component={P3DataPractice} />
       <Route path="/practice/p3-parallel-perpendicular" component={P3GeometryPractice} />
+      <Route path="/practice/p3-mixed-steps" component={P3CorePractice} />
+      <Route path="/practice/p3-parallelogram-trapezium" component={P3CorePractice} />
       <Route path="/practice/p3-mixed-operations" component={P3Practice} />
       <Route path="/practice/p4-fractions-decimals" component={P4Practice} />
       <Route path="/practice/p4-factors-multiples" component={P4FactorsPractice} />
